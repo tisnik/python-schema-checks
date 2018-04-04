1adb677f6bd2420c5c8b0b9e99e023c5d47504fc
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-04 08:58:11 +0200

    Schema lib demo #2

---
+++ schema-demo-2/run.sh
@@ -0,0 +1,1 @@
+python3 schema-demo-2.py
---
+++ schema-demo-2/schema-demo-2.py
@@ -0,0 +1,53 @@
+from schema import Schema, SchemaError
+
+
+def validate(schema, data):
+    try:
+        print("\n\n")
+        print(schema)
+        print(data)
+        schema.validate(data)
+        print("pass")
+    except SchemaError as e:
+        print(e)
+
+
+def pos(value):
+    return type(value) is int and value > 0
+
+
+number_list = Schema([int, float, complex])
+
+validate(number_list, [1, 2, 3])
+validate(number_list, [1.1, 2.2, 3.3])
+validate(number_list, [1+2j, 3+4j, 5j])
+validate(number_list, ["1", "2", "3"])
+
+binary_numbers = Schema([0, 1])
+validate(binary_numbers, [0, 0, 0])
+validate(binary_numbers, [1, 1, 0])
+validate(binary_numbers, [1, 2, 3])
+
+validate(Schema(pos), 42)
+validate(Schema(pos), 0)
+validate(Schema(pos), -1)
+validate(Schema(pos), 1.5)
+
+validate(Schema(lambda value: value < 0), 42)
+validate(Schema(lambda value: value < 0), 0)
+validate(Schema(lambda value: value < 0), -1)
+
+user = Schema({"name": str,
+               "surname": str,
+               "id": pos})
+
+validate(user, {"name": "Eda",
+                "surname": "Wasserfall",
+                "id": 1})
+
+validate(user, {"name": "Eda",
+                "id": 1})
+
+validate(user, {"name": "Eda",
+                "surname": "Wasserfall",
+                "id": 0})
