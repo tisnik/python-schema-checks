f490640eabc7b739e24c95fa0fa9adb0ae513a0d
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-04 09:08:44 +0200

    Schemagic demo #5

---
+++ schemagic-demo-5/schemagic_test.py
@@ -0,0 +1,33 @@
+import sys
+import traceback
+from schemagic import validate_against_schema
+
+
+def validate(schema, data):
+    try:
+        print("\n\n")
+        print(schema)
+        print(data)
+        validate_against_schema(schema, data)
+        print("pass")
+    except (ValueError, TypeError) as e:
+        print(e)
+
+
+def is_type(value, expected_type):
+    actual_type = type(value)
+    if actual_type is not expected_type:
+        msg = "Expected type: {expected}, but the value has type {actual}".format(
+            expected=expected_type, actual=actual_type)
+        raise TypeError(msg)
+
+
+def is_int(value):
+    is_type(value, int)
+
+
+string_to_int_map = {str:is_int}
+
+validate(string_to_int_map, {"prvni": 1, "druha": 2, "treti": 3})
+validate(string_to_int_map, {"prvni": 1.5, "druha": "2", "treti": 3})
+validate(string_to_int_map, {"prvni": "x", "druha": "y", "treti": "z"})
