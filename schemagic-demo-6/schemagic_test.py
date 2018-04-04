dd40ff5a39506fd4f41348d35e4ab294f68236cb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-05 00:15:45 +0200

    Schemagic demo #6

---
+++ schemagic-demo-6/schemagic_test.py
@@ -0,0 +1,60 @@
+import sys
+import traceback
+import re
+from schemagic import validate_against_schema
+
+
+def validate(schema, data):
+    try:
+        print("\n\n")
+        print(schema)
+        print(data)
+        print(validate_against_schema(schema, data))
+        print("pass")
+    except (ValueError, TypeError) as e:
+        print(e)
+
+
+def is_type(value, expected_type):
+    actual_type = type(value)
+    if actual_type is not expected_type:
+        msg = "Expected type: {expected}, but the value has type {actual}".format(
+            expected=expected_type, actual=actual_type)
+        raise TypeError(msg)
+
+
+def name_str(value):
+    if not re.fullmatch("[A-Z][a-z]+", value):
+        msg = "Proper name expected, but got '{value}' instead".format(value=value)
+        raise TypeError(msg)
+
+
+def pos_int(value):
+    is_type(value, int)
+    if value <= 0:
+        msg = "Positive number expected, but got {value} instead".format(value=value)
+        raise TypeError(msg)
+
+
+user = {"name": name_str,
+        "surname": name_str,
+        "id": pos_int}
+
+validate(user, {"name": "Eda",
+                "surname": "Wasserfall",
+                "id": 1})
+
+validate(user, {"name": "eda",
+                "surname": "Wasserfall",
+                "id": 1})
+
+validate(user, {"name": "E",
+                "surname": "Wasserfall",
+                "id": 1})
+
+validate(user, {"name": "Eda",
+                "id": 1})
+
+validate(user, {"name": "Eda",
+                "surname": "Wasserfall",
+                "id": 0})
