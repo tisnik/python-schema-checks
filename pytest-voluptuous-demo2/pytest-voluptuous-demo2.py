6e6a0b1ef856719734991137f2c875b5d85a71d2
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-17 09:10:59 +0200

    Demo #2 for the pytest-voluptuous library

---
+++ pytest-voluptuous-demo2/pytest-voluptuous-demo2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# vim: set fileencoding=utf-8
+
+from pytest_voluptuous import S, Partial, Exact
+from voluptuous import Invalid
+from voluptuous.validators import All, Length
+
+
+def pos(value):
+    if type(value) is not int or value <= 0:
+        raise Invalid("positive integer value expected, but got {v} instead".format(v=value))
+
+
+user = S({"name": str,
+          "surname": str,
+          "id": pos})
+
+
+def test1():
+    assert user == {"name": "Eda",
+                    "surname": "Wasserfall",
+                    "id": 1}
+
+
+def test2():
+    assert user == {"name": "Eda",
+                    "id": 1}
+
+
+def test3():
+    assert user == {"name": "Eda",
+                    "surname": "Wasserfall",
+                    "id": 0}
