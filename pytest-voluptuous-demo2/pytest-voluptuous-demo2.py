514ea2138b6d5cd940a4777968a9daa08a58d1c6
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-15 14:16:14 +0100

    Fixed issue #53

--- pytest-voluptuous-demo2/pytest-voluptuous-demo2.py
+++ pytest-voluptuous-demo2/pytest-voluptuous-demo2.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2018  Pavel Tisnovsky
+#  (C) Copyright 2018, 2021  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -47,7 +47,7 @@ def test3():
 
 users = S([S({"name": str,
              "surname": str,
-             "id": pos})])
+              "id": pos})])
 
 def test4():
     assert users == [{"name": "Eda",
