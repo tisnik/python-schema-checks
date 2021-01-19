59ac885a6c456c547cc4d0de0700eb7cedc3c57d
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-19 08:30:45 +0100

    Fixed issue #10

--- schema-demo-5/schema-demo-5.py
+++ schema-demo-5/schema-demo-5.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf-8
 
 #
-#  (C) Copyright 2018  Pavel Tisnovsky
+#  (C) Copyright 2018, 2021  Pavel Tisnovsky
 #
 #  All rights reserved. This program and the accompanying materials
 #  are made available under the terms of the Eclipse Public License v1.0
@@ -47,7 +47,8 @@ POSITIONS = ["QA", "DevOps", "Admin", "Docs", "HR"]
 employee = Schema({"name": And(str, len, name_str),
                    "surname": And(str, len, name_str),
                    "id": positive_integer,
-                   Optional("salary"): And(Or(positive_integer, positive_float), lambda x: x > 10000.0 and x < 99999.0),
+                   Optional("salary"): And(Or(positive_integer, positive_float),
+                                           lambda x: x > 10000.0 and x < 99999.0),
                    Optional("position"): And(str, lambda s: s in POSITIONS)})
 
 verbose_mode = "-v" in argv
