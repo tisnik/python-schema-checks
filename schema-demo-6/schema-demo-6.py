682fadbc56e2bc941f3198b6d06cca93e4d57510
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-20 09:07:17 +0100

    Fixed issue #48

--- schema-demo-6/schema-demo-6.py
+++ schema-demo-6/schema-demo-6.py
@@ -162,7 +162,7 @@ validate(employee, {"name": "Eda",
                     "id": 14,
                     "salary": 45000.0,
                     "position": "HR"},
-                    verbose_mode)
+         verbose_mode)
 
 validate(employee, {"name": "Eda",
                     "surname": "Unique",
