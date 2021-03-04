d9d0a17a8a9abcceffc5a2a186deaece9fd4b2d4
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-03-04 10:11:08 +0100

    Fixed issue #142

--- schema-demo-6/schema-demo-6.py
+++ schema-demo-6/schema-demo-6.py
@@ -21,6 +21,7 @@ from schema import Schema, SchemaError, And, Or, Optional, Regex
 
 
 def validate(schema, data, verbose_mode=False):
+    """Function that performs schema check validation."""
     try:
         print("\n\n")
         if verbose_mode:
